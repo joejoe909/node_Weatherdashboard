dd222e4f575466db3454f426c4734df69badcc17
Joe Farrish <joseph_a_f@hotmail.com>
2020-12-09 10:23:17 -0700

    I Was able to set proper behavior on the sidebar

--- public/js/FE_Logic.js
+++ public/js/FE_Logic.js
@@ -52,28 +52,25 @@ $(document).ready(function () {
         console.log('click');
         const cityStr = $("#cityInpt").val();
         console.log("searching " + cityStr);
-       
-        //check if city name is in the history list if it is dont add the name
-        const cities = $("#tabelBody").contents().filter(function(){ 
-            return this.nodeType === 3;  //
-        });
-        
-        console.log("cities is type of: " + typeof(cities))
-        let found = false;
-        for(let i = 0; i < cities.length; i++)
-        {   
-            console.log("searching" + String(cities[i]))
-            if(String(cities[i]) === cityStr) found ===true;
+ 
+        //check if city is already in the list if is don't add it. 
+        let tbody = document.getElementById("tabelBody");
+        let cells = tbody.getElementsByTagName("h5");
+ 
+        let found = false
+        let city;
+        for(let i = 0; i < cells.length; i++)
+        {
+            if(cityStr === cells[i].innerText){
+                 found = true;
+                 city = cells[i].innerText;
+            }
         }
 
         if(!found){
-            const cty = '<tr>' + '<td id="' + cityStr + '">' + cityStr + '</td>' + '</tr>'
-            $('#tabelBody').prepend(cty);    
-        }else{
-            console.log("found!")
+            const cty = '<tr>' + '<td id="' + city + '">' + '<h5>' + cityStr + '</h5>' + '</td>' + '</tr>'
+            $('#tabelBody').prepend(cty); 
         }
-
-
         buildQueryURL(cityStr);
 
     });
@@ -82,7 +79,7 @@ $(document).ready(function () {
     $("#cityList").on("click", "tr", function () { //event delegation!
         var city = $(this).attr('id');
         cityString = $(this).text();
-        cityString = cityString.trim()//cityString.replace(/\s/g, ''); // remove spaces introduced from back end
+        cityString = cityString.trim()//cityString.replace(/\s/g, ''); // remove spaces introduced from back end        
         buildQueryURL(cityString);
 
     });
